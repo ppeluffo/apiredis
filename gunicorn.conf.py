8eddf0e1ad6a7dd920f3e22feeca4e0db999dbcd
Pablo Peluffo <ppeluffo@spymovil.com>
2025-10-11 09:56:50 -0300

    20251011

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,14 +4,17 @@ import multiprocessing
 
 max_requests = 1000
 max_requests_jitter = 50
+
 # Si queremos el log en stdout
-log_file = "-"
+
 #workers = multiprocessing.cpu_count() * 2 + 1
 workers = 10
-#bind = '0.0.0.0:5100'
-#worker_class = 'sync'
+
+# Enviar logs a stdout/stderr
+accesslog = '-'      # stdout
+errorlog = '-'       # stderr
 loglevel = 'info'
-#accesslog = '/var/log/gunicorn/apiredis.log'
-#acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
-acceslogformat =""
-#errorlog =  '/var/log/gunicorn/apiredis.log'
+
+# Opcional: formato de access log
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+#acceslogformat = ""
